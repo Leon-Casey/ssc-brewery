0bafdfecc39dd7537dcb6a514b216ede0501f979
Leon Casey <43139614+Leon-Casey@users.noreply.github.com>
2021-08-31 18:31:00 +0200

    progress

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -43,14 +43,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeRequests(authorize -> {
-                    authorize
-                            .antMatchers("/h2-console/**").permitAll() //do not use in prod
-                            .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
-                            .antMatchers("/beers/find", "/beers*").permitAll()
-                            .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
-                            .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
-                            .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
-                })
+            authorize
+                    .antMatchers("/h2-console/**").permitAll() //do not use in prod
+                    .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                    .antMatchers("/beers/find", "/beers*").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                    .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**").hasRole("ADMIN")
+                    .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
+                    .mvcMatchers("/brewery/breweries")
+                    .hasAnyRole("ADMIN", "CUSTOMER")
+                    .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
+                    .hasAnyRole("ADMIN", "CUSTOMER");
+        })
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
